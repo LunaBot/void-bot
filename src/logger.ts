512dd15f52068e430d4f9fa987ab47b5bd8b1c00
Alexis Tyler <xo@wvvw.me>
2021-04-22 17:00:22 +0930

    fix: broken logger

--- src/logger.ts
+++ src/logger.ts
@@ -2,12 +2,12 @@ import chalk from 'chalk';
 
 export const logger = {
     info(message: string, ...optionalParams: any[]) {
-        console.info.bind(console)(`[${chalk.cyan('INFO')}]%s${optionalParams.length === 0 ? '' : '[%s]'}`, message, ...optionalParams);
+        console.info.bind(console)(`[${chalk.cyan('INFO')}]${message}`, ...optionalParams);
     },
     debug(message: string, ...optionalParams: any[]) {
-        console.debug.bind(console)(`[${chalk.magenta('DEBUG')}]%s${optionalParams.length === 0 ? '' : '[%s]'}`, message, ...optionalParams);
+        console.debug.bind(console)(`[${chalk.magenta('DEBUG')}]${message}`, ...optionalParams);
     },
     error(error: Error | string, ...optionalParams: any[]) {
-        console.error.bind(console)(`[${chalk.red('ERROR')}]%s${optionalParams.length === 0 ? '' : '[%s]'}`, error, ...optionalParams);
+        console.error.bind(console)(`[${chalk.red('ERROR')}]`, error, ...optionalParams);
     }
 };
